e3bebbe97c2513f5aa1f6d1704bce1d80e1d188c
jiHwan <ghkdrbqhd35@gmail.com>
2024-07-26 17:20:29 +0900

    #01 공지사항 검색 컴포넌트 등록

---
+++ src/pages/Notice.tsx
@@ -0,0 +1,9 @@
+import { NoticeSearch } from '../component/page/Notice/NoticeSearch/NoticeSearch';
+
+export const Notice = () => {
+    return (
+        <>
+            <NoticeSearch />
+        </>
+    );
+};
--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -2,6 +2,7 @@ import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { DashBoard } from '../component/layout/DashBoard/DashBoard';
 import { NotFound } from '../component/common/NotFound/NotFound';
+import { Notice } from '../pages/Notice';
 
 const routers: RouteObject[] = [
     { path: '*', element: <NotFound /> },
@@ -9,6 +10,7 @@ const routers: RouteObject[] = [
     {
         path: '/react',
         element: <DashBoard />,
+        children: [{ path: 'board', children: [{ path: 'notice.do', element: <Notice /> }] }],
     },
 ];
 
